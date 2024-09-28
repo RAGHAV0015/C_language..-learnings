89cb6fc568be2fe482622a2bf681fb78884dbcae
RAGHAV0015 <raghavsumanth0015@gmail.com>
2024-09-29 00:19:33 +0530

    Added the correct code to the file

---
+++ square.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int a;
+    printf("enter the value of side of quare:");
+    scanf("%d",&a);
+    int area =a * a;
+    printf("the area of quare is:%d\n",area);
+    return 0;
+
+}
