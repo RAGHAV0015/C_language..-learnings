06061bf50435b1b3b4056d4a771e08a173346a66
RAGHAV0015 <raghavsumanth0015@gmail.com>
2024-09-28 23:38:32 +0530

    Added the correct code to the file

---
+++ my5th.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter a");
+    scanf("%d",&a);
+    printf("enter the b");
+    scanf("%d",&b);
+    int sum =a+b;
+    printf("%d",sum);
+    return 0;
+
+}
